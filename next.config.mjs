4c49ee82545034222b7cf0ed45dd212182b3ab48
Raphael Pereira Nascimento <raphael.nascimento.dev@gmail.com>
2026-07-01 11:00:28 -0300

    adiciona dominios das imagens ao remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,21 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "plus.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.jsdelivr.net",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "cdn.jsdelivr.net" },
+      { protocol: "https", hostname: "livesport-ott-images.ssl.cdn.cra.cz" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "img.nsctotal.com.br" },
+      { protocol: "https", hostname: "images.copaamerica.com" },
+      { protocol: "https", hostname: "lncimg.lance.com.br" },
+      { protocol: "https", hostname: "media.squawka.com" },
+      { protocol: "https", hostname: "cdn.assets-casacor.tec.br" },
+      { protocol: "https", hostname: "library.sportingnews.com" },
+      { protocol: "https", hostname: "p2.trrsf.com" },
+      { protocol: "https", hostname: "selecaopiemonte.com.br" },
+      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
+      { protocol: "https", hostname: "viagem.cnnbrasil.com.br" },
     ],
   },
 };
